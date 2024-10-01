bb9e0fc185dabe9529325f412bd9a909ef7f0621
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-10-01 10:11:36 -0300

    Add
    Add

---
+++ TacDri/TacDri/src/main/java/com/TacDri/TacDri/TacDriApplication.java
@@ -0,0 +1,13 @@
+package com.TacDri.TacDri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacDriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacDriApplication.class, args);
+	}
+
+}
---
+++ TacDri/TacDri/src/test/java/com/TacDri/TacDri/TacDriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TacDri.TacDri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacDriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
